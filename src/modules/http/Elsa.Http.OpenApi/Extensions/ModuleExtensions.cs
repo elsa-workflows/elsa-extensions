15d5ea80d03f38a3174a3db4c09a3407faea661e
cristinamudura <cristina.mudura@nexxbiz.io>
2025-08-28 10:59:51 +0200

    Refactor HTTP OpenAPI extension methods: update method signatures and improve documentation

--- src/modules/http/Elsa.Http.OpenApi/Extensions/ModuleExtensions.cs
+++ src/modules/http/Elsa.Http.OpenApi/Extensions/ModuleExtensions.cs
@@ -1,23 +1,22 @@
+using Elsa.Extensions;
 using Elsa.Features.Services;
+using Elsa.Http.Features;
 using Elsa.Http.OpenApi.Features;
 
 // ReSharper disable once CheckNamespace
 namespace Elsa.Extensions;
 
 /// <summary>
-/// Extension methods for configuring HTTP OpenAPI features on modules.
+/// Provides extensions to install the HTTP OpenAPI feature.
 /// </summary>
 public static class ModuleExtensions
 {
     /// <summary>
-    /// Installs the HTTP OpenAPI feature.
+    /// Install the <see cref="HttpOpenApiFeature"/> feature as part of the HTTP feature configuration.
     /// </summary>
-    /// <param name="module">The module to install the feature on.</param>
-    /// <param name="configure">Optional configuration action for HttpOpenApiFeature.</param>
-    /// <returns>The module for chaining.</returns>
-    public static IModule UseHttpOpenApi(this IModule module, Action<HttpOpenApiFeature>? configure = default)
+    public static HttpFeature UseOpenApi(this HttpFeature feature, Action<HttpOpenApiFeature>? configure = default)
     {
-        module.Configure(configure);
-        return module;
+        feature.Module.Configure(configure);
+        return feature;
     }
 }
