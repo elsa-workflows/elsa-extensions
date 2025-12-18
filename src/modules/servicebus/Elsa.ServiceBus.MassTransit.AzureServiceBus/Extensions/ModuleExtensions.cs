6700f2b7e298cf10f61b67c77b5ce45c0345d3df
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-12-18 10:21:00 +0100

    Add overloads to `UseAzureServiceBus` for enhanced configuration flexibility.

--- src/modules/servicebus/Elsa.ServiceBus.MassTransit.AzureServiceBus/Extensions/ModuleExtensions.cs
+++ src/modules/servicebus/Elsa.ServiceBus.MassTransit.AzureServiceBus/Extensions/ModuleExtensions.cs
@@ -1,7 +1,10 @@
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
 using Elsa.Features.Services;
 using Elsa.ServiceBus.MassTransit.AzureServiceBus.Features;
 using Elsa.ServiceBus.MassTransit.Features;
 using JetBrains.Annotations;
+using MassTransit;
 
 // ReSharper disable once CheckNamespace
 namespace Elsa.Extensions;
@@ -12,19 +15,61 @@ namespace Elsa.Extensions;
 [PublicAPI]
 public static class ModuleExtensions
 {
-    /// <summary>
-    /// Enable and configure the Azure Service Bus transport for MassTransit.
-    /// </summary>
-    public static MassTransitFeature UseAzureServiceBus(this MassTransitFeature feature, string? connectionString, Action<AzureServiceBusFeature>? configure = null)
+    extension(MassTransitFeature feature)
     {
-        feature.Module.Configure((Action<AzureServiceBusFeature>)Configure);
-        return feature;
-
-        void Configure(AzureServiceBusFeature bus)
+        /// <summary>
+        /// Enable and configure the Azure Service Bus transport for MassTransit.
+        /// </summary>
+        /// <param name="connectionString">The Azure Service Bus connection string or name.</param>
+        /// <param name="configure">An optional action to further configure the Azure Service Bus feature.</param>
+        /// <returns>The <see cref="MassTransitFeature"/> for method chaining.</returns>
+        public MassTransitFeature UseAzureServiceBus(string? connectionString, Action<AzureServiceBusFeature>? configure = null)
         {
-            bus.AzureServiceBusOptions = options => options.ConnectionStringOrName = connectionString;
-            bus.ConnectionString = connectionString;
-            configure?.Invoke(bus);
+            feature.Module.Configure((Action<AzureServiceBusFeature>)Configure);
+            return feature;
+
+            void Configure(AzureServiceBusFeature bus)
+            {
+                bus.AzureServiceBusOptions = options => options.ConnectionStringOrName = connectionString;
+                bus.ConnectionString = connectionString;
+                configure?.Invoke(bus);
+            }
+        }
+        
+        /// <summary>
+        /// Enable and configure the Azure Service Bus transport for MassTransit.
+        /// </summary>
+        /// <param name="hostAddress">The host address, in MassTransit format (sb://namespace.servicebus.windows.net/scope).</param>
+        /// <param name="serviceBusClient">The Azure Service Bus client instance.</param>
+        /// <param name="serviceBusAdministrationClient">The Azure Service Bus administration client instance.</param>
+        /// <param name="configure">An optional action to further configure the Azure Service Bus feature.</param>
+        /// <returns>The <see cref="MassTransitFeature"/> for method chaining.</returns>
+        public MassTransitFeature UseAzureServiceBus(
+            Uri hostAddress, 
+            ServiceBusClient serviceBusClient, 
+            ServiceBusAdministrationClient serviceBusAdministrationClient, 
+            Action<AzureServiceBusFeature>? configure = null)
+        {
+            feature.Module.Configure((Action<AzureServiceBusFeature>)Configure);
+            return feature;
+
+            void Configure(AzureServiceBusFeature bus)
+            {
+                bus.ServiceBusAdministrationClientFactory = _ => serviceBusAdministrationClient;
+                bus.ConfigureTransportBus += (_, configurator) => configurator.Host(hostAddress, serviceBusClient, serviceBusAdministrationClient);
+                configure?.Invoke(bus);
+            }
+        }
+
+        /// <summary>
+        /// Enable and configure the Azure Service Bus transport for MassTransit.
+        /// </summary>
+        /// <param name="configure">An action to configure the Azure Service Bus feature.</param>
+        /// <returns>The <see cref="MassTransitFeature"/> for method chaining.</returns>
+        public MassTransitFeature UseAzureServiceBus(Action<AzureServiceBusFeature>? configure)
+        {
+            feature.Module.Configure(configure);
+            return feature;
         }
     }
 }
