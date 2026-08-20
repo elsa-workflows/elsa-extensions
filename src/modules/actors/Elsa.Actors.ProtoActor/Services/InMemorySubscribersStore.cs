391bca34b1b2c093cf605a2899280f1811f29780
Rostislav Statko <contact@dendeline.com>
2026-08-20 10:15:26 +0300

    fix: support ct in InMemorySubscribersStore

--- src/modules/actors/Elsa.Actors.ProtoActor/Services/InMemorySubscribersStore.cs
+++ src/modules/actors/Elsa.Actors.ProtoActor/Services/InMemorySubscribersStore.cs
@@ -10,18 +10,21 @@ internal sealed class InMemorySubscribersStore : IKeyValueStore<Subscribers>
 
     public Task<Subscribers> GetAsync(string id, CancellationToken ct)
     {
+        ct.ThrowIfCancellationRequested();
         _store.TryGetValue(id, out var subscribers);
         return Task.FromResult(subscribers ?? new Subscribers());
     }
 
     public Task SetAsync(string id, Subscribers state, CancellationToken ct)
     {
+        ct.ThrowIfCancellationRequested();
         _store[id] = state;
         return Task.CompletedTask;
     }
 
     public Task ClearAsync(string id, CancellationToken ct)
     {
+        ct.ThrowIfCancellationRequested();
         _store.TryRemove(id, out _);
         return Task.CompletedTask;
     }
